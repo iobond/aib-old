393b3d5af111157524cafc185cfc1fbd7b5672aa
temple <temple@hulamaptech.com>
2016-08-18 22:43:06 -0700

    fix toolbar icon, fix year for copyright

--- src/wtmint.h
+++ src/wtmint.h
@@ -15,7 +15,7 @@
 #define CLIENT_VERSION_MAJOR       3
 #define CLIENT_VERSION_MINOR       0
 #define CLIENT_VERSION_REVISION    0
-#define CLIENT_VERSION_BUILD       1
+#define CLIENT_VERSION_BUILD       2
 
 #define WTMINT_MAX_MONEY			 3141592653589793238
                                    // 271828182845904523  
